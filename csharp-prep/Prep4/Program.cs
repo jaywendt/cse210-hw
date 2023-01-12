666f92729f13b2eab6609b01a1255f66ce7ec7e1
jaywendt <wen16005@byui.edu>
2023-01-11 18:59:16 -0800

    csharp prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Please enter your numbers here. Type 0 once finished.  ");
+
+            string enter = Console.ReadLine();
+            number = int.Parse(enter);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int listnumber in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of this list is {sum}.");
+
+        float avg = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {avg}.");
+
+        int max = numbers[0];
+
+        foreach (int listnumber in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}.");
+
     }
 }
