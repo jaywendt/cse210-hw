e7eb81450ac96d953a39f4f104d1ba067bd990e1
jaywendt <wen16005@byui.edu>
2023-01-11 17:50:04 -0800

    csharp prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        // Console.Write("Welcome to my guessing machine!  What is the magic number? ");
+        // int number = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low!");
+            }
+
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
+
+    }    
 }
