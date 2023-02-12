e1a649f4805f7f795cc4ad0971317459cc8e8351
jaywendt <wen16005@byui.edu>
2023-02-12 13:00:35 -0800

    proj

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
+class Scripture
+{
+    public string text;
+    public string referenceNam;
+    public int referenceNum;
+
+    public void post()
+    {
+        Console.WriteLine(referenceNam + ':' + referenceNum + ':' + text);
+    }
+}
+
+class Text
+{
+
+}
+
+class Commands
+{
+    public void quit()
+    {
+        Console.Clear();
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scrip1 = new Scripture();
+        scrip1.text = "Behold, I am a disciple of Jesus Christ, the Son of God.  I have been called of him to declare his word among his people. that they might have everlasting life.";
+        scrip1.referenceNam = "Third Nephi 5";
+        scrip1.referenceNum = 13;
+
+        scrip1.post();
+        
+        Console.WriteLine("Press Enter to continue or type 'quit' to finish");
+        Console.ReadLine();
+
+        Console.Clear();
+
+        Console.WriteLine();
+
     }
 }
